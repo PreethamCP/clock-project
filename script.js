c23a124977b1b6169a2f9006e874a149aabeeddc
Preetham <138687129+PreethamCP@users.noreply.github.com>
2023-08-19 11:16:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+setInterval(() => {
+  let currentTime = new Date(); // gets the current date and time
+  hrs.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours(); //inline if conditions if less than 10 add 0  else nothing
+  min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes(); // sends the current minutes to the id min
+  sec.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds(); // send the current seconds to the id sec
+}, 1000);
 
